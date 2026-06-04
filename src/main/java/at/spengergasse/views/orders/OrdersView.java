3ad480cf01f53ec0440e6d2c43412627d88eda9c
Dominic Neufeld <neu250722@spengergasse.at>
2026-06-04 15:09:46 +0200

    Grid addColumn

--- src/main/java/at/spengergasse/views/orders/OrdersView.java
+++ src/main/java/at/spengergasse/views/orders/OrdersView.java
@@ -6,10 +6,12 @@ import at.spengergasse.service.FurnitureOrderService;
 import com.vaadin.flow.component.ClickEvent;
 import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -20,6 +22,8 @@ import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.vaadin.lineawesome.LineAwesomeIconUrl;
 
+import java.time.LocalDate;
+
 @PageTitle("Orders")
 @Route("orders")
 @Menu(order = 1, icon = LineAwesomeIconUrl.FILE)
@@ -29,7 +33,7 @@ public class OrdersView extends VerticalLayout {
     private final Button buttonAdd1Euro = new Button("Add 1 Euro");
     private final Button buttonRemoveAllAssemblyServices = new Button("Remove assembly services");
     private final Button buttonAddWrong = new Button("Add Wrong");
-    private final Grid<FurnitureProduct> grid = new Grid<>(FurnitureProduct.class, true);
+    private final Grid<FurnitureProduct> grid = new Grid<>(FurnitureProduct.class, false);
     private final FurnitureOrderService furnitureOrderService;
 
     public OrdersView(@Autowired FurnitureOrderService furnitureOrderService) {
@@ -45,6 +49,46 @@ public class OrdersView extends VerticalLayout {
         buttonRemoveAllAssemblyServices.addClickListener(event -> removeAllAssemblyServices());
         buttonAddWrong.addClickListener(event -> addWrongFurnitureProduct());
         add(new HorizontalLayout(buttonRemoveAllOrders, buttonAdd10Orders, buttonAdd1Euro, buttonRemoveAllAssemblyServices, buttonAddWrong));
+
+        grid.addColumn(furnitureProduct -> furnitureProduct.getFurnitureId())
+            .setHeader("Product Number")
+            .setSortable(true);
+        grid.addColumn(furnitureProduct -> furnitureProduct.getProductionDate())
+            .setHeader("Product Number")
+            .setSortable(true);
+        grid.addColumn(furnitureProduct -> furnitureProduct.getProductName())
+            .setHeader("Product Name")
+            .setSortable(true);
+
+        Image l = new Image("icons/materialtype.png", "Material Logo");
+        l.setWidth("42px");
+        HorizontalLayout headerType = new HorizontalLayout(l, new Span("Type"));
+
+        grid.addColumn(furnitureProduct -> furnitureProduct.getMaterial())
+            .setHeader(headerType)
+            .setSortable(true);
+        grid.addColumn(furnitureProduct -> furnitureProduct.getPrice())
+            .setHeader("Price EUR")
+            .setSortable(true);
+        grid.addColumn(furnitureProduct -> furnitureProduct.getQuantity())
+            .setHeader("Product Quantity")
+            .setSortable(true);
+        grid.addColumn(furnitureProduct -> furnitureProduct.getAssemblyService())
+            .setHeader("Product Assembly")
+            .setSortable(true);
+        grid.addColumn(furnitureProduct -> (furnitureProduct.getAssemblyService()? "to be assembled" : "NO assembly"))
+            .setHeader("Assembly Service")
+            .setSortable(true);
+        grid.addComponentColumn(furnitureProduct -> {
+                    Checkbox assembly = new Checkbox(furnitureProduct.getAssemblyService());
+                    assembly.setReadOnly(true);
+                    return assembly;
+                })
+            .setHeader("Assembly Service")
+            .setSortable(true);
+
+
+
 
         add(grid);
         reload();
