e857859d79a4cab92ee3584b2ed93e632c9c3162
Dominic Neufeld <neu250722@spengergasse.at>
2026-06-09 13:02:07 +0200

    Validation

--- src/main/java/at/spengergasse/domain/FurnitureProduct.java
+++ src/main/java/at/spengergasse/domain/FurnitureProduct.java
@@ -2,6 +2,7 @@ package at.spengergasse.domain;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -18,16 +19,30 @@ import java.util.concurrent.atomic.AtomicLong;
 public class FurnitureProduct {
     @Id
     private Long furnitureId;
+    @NotNull(message = "Furniture production date is required!")
+    @PastOrPresent(message = "Furniture production only in the past or present!")
     private LocalDate productionDate;
+    @NotBlank(message = "Product name is required!")
+    @Size(min=3, max=100, message = "Wrong name")
     private String    productName;
+    @NotNull(message = "Material is required!")
+    @Pattern(
+            regexp = "Plastic|Wood|Metal|Customized",
+            message = "Material must be Plastic, Wood, Metal or Customized"
+    )
     private String    material;
+    @NotNull(message = "Price is required!")
+    @DecimalMin(value = "500.0", message = "Minimum 500 EUR")
+    @DecimalMax(value = "10000.0", message = "Maximal 10.000 EUR")
     private Double    price;
+    @NotNull(message = "Quantity is required!")
+    @Min(value = 1, message = "Minimum 1 piece!")
+    @Max(value = 1000, message = "Maximum 1000 pieces!")
     private Integer   quantity;
+    @NotNull(message = "Assembly information is required!")
     private Boolean   assemblyService;
 
     private static final AtomicLong sequence = new AtomicLong(1000);
-    private static final String[] materialTypes = {"Plastic", "Wood", "Metal", "Customized"};
-
 
     public FurnitureProduct() {
     }
@@ -65,8 +80,6 @@ public class FurnitureProduct {
     }
 
     public void setFurnitureTypes(String materialType) {
-        if (! Arrays.asList(materialTypes).contains(materialType))
-            throw new FurnitureProductException("Unknown furniture Type!");
         this.material = materialType;
     }
 
